70b49c90bc211bc94a0d65df6613634e40974adf
George Miracle <97882379+georgemiraclee@users.noreply.github.com>
2025-01-09 11:28:19 +0700

    feat: make a function, app

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { ZegoUIKitPrebuilt } from '@zegocloud/zego-uikit-prebuilt';
+import { Element } from './type';
+import { getUrlParams, randomID } from './functions';
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+      const roomID = getUrlParams().get('roomID') || randomID(5);
+      const myMeeting = async (element: Element) => {
+     // generate Kit Token
+      const appID = 49825981;
+      const serverSecret = "974f780a0e28d5726b3e7c7e75da1e72";
+      const kitToken =  ZegoUIKitPrebuilt.generateKitTokenForTest(appID, serverSecret, roomID,  randomID(5),  randomID(5));
+
+    
+     // Create instance object from Kit Token.
+      const zp = ZegoUIKitPrebuilt.create(kitToken);
+      // start the call
+      zp.joinRoom({
+        container: element,
+        sharedLinks: [
+          {
+            name: 'Personal link',
+            url:
+             window.location.protocol + '//' + 
+             window.location.host + window.location.pathname +
+              '?roomID=' +
+              roomID,
+          },
+        ],
+        scenario: {
+          mode: ZegoUIKitPrebuilt.GroupCall, // To implement 1-on-1 calls, modify the parameter here to [ZegoUIKitPrebuilt.OneONoneCall].
+        },
+      });
+
+    
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div
+      className="myCallContainer"
+      ref={myMeeting}
+      style={{ width: '100vw', height: '100vh' }}
+    ></div>
+  );
 }
-
-export default App
---
+++ src/functions.ts
@@ -0,0 +1,20 @@
+export function randomID(len: number) {
+  let result = '';
+  if (result) return result;
+  const chars = '12345qwertyuiopasdfgh67890jklmnbvcxzMNBVCZXASDQWERTYHGFUIOLKJP',
+    const maxPos = chars.length;
+    let i;
+
+  len = len || 5;
+  for (i = 0; i < len; i++) {
+    result += chars.charAt(Math.floor(Math.random() * maxPos));
+  }
+  return result;
+}
+
+export function getUrlParams(
+  url = window.location.href
+) {
+  const urlStr = url.split('?')[1];
+  return new URLSearchParams(urlStr);
+}
